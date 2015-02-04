91b3f60d163c692f9fead37d4ad1049886564efd
martji <martine0092@gmail.com>
2015-02-04 10:19:22 +0800

    Commit message 1. create folder message 2. commit the message class

---
+++ message/protocl/copy.bat
@@ -0,0 +1,4 @@
+xcopy D:\java_project\protocl\messageBody\*.* D:\java_project\Memcached\src\messageBody /s /y
+xcopy D:\java_project\protocl\messageBody\*.* D:\java_project\User\src\messageBody /s /y
+xcopy D:\java_project\protocl\messageBody\*.* D:\java_project\WebServer\src\messageBody /s /y
+pause
