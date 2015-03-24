9db6425f0244f63fe7154ce516dd8ef5014c71ed
martji <magq1001@gmail.com>
2015-03-24 21:46:40 +0800

    Bug fix 1. modify the code and fix some bug

---
+++ rmemcached_client/src/main/java/com/sdp/client/MemcachedClient.java
@@ -0,0 +1,111 @@
+package com.sdp.client;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import com.sdp.server.ServerNode;
+/**
+ * 
+ * @author martji
+ *
+ */
+
+public class MemcachedClient {
+
+	int clientId;
+	int replicasNum;
+	Map<Integer, MClient> clientMap = new HashMap<Integer, MClient>();
+	
+	/**
+	 * 
+	 * @param clientId
+	 * @param replicasNum
+	 * @param serversMap
+	 */
+	public MemcachedClient(int clientId, int replicasNum, Map<Integer, ServerNode> serversMap) {
+		this.clientId = clientId;
+		this.replicasNum = replicasNum;
+		init(serversMap);
+	}
+	
+	public MemcachedClient(int clientId, int replicasNum) {
+		this.clientId = clientId;
+		this.replicasNum = replicasNum;
+	}
+	
+	public void init(Map<Integer, ServerNode> serversMap) {
+		Collection<ServerNode> serverList = serversMap.values();
+		for (ServerNode serverNode : serverList) {
+			int serverId = serverNode.getId();
+			String host = serverNode.getHost();
+			int port = serverNode.getPort();
+			
+			MClient mClient = new MClient(clientId, host, port);
+			clientMap.put(serverId, mClient);
+		}
+	}
+	
+	public void shutdown() {
+		Collection<MClient> clientList = clientMap.values();
+		for (MClient mClient : clientList) {
+			mClient.shutdown();
+		}
+	}
+	
+	public MClient randMClient(Integer hash) {
+		if (replicasNum == 1) {
+			return leaderClient(hash);
+		}
+		Random random = new Random();
+		int clientsNum = clientMap.size();
+		int leaderIndex = hash % clientsNum + clientsNum;
+		int index = random.nextInt(replicasNum);
+		for (int i = 0; i < replicasNum; i++) {
+			int num = (leaderIndex + i + index) % clientsNum;
+			if (num < 0) {
+				num += clientsNum;
+			}
+			MClient mClient = clientMap.get(num);
+			if (mClient != null) {
+				return mClient;
+			}
+		}
+		return null;
+	}
+	
+	public MClient leaderClient(Integer hash) {
+		int clientsNum = clientMap.size();
+		int leaderIndex = hash % clientsNum + clientsNum;
+		for (int i = 0; i < replicasNum; i++) {
+			int num = (leaderIndex + i)	% clientsNum;
+			if (num < 0) {
+				num += clientsNum;
+			}
+			MClient mClient = clientMap.get(num);
+			if (mClient != null) {
+				return mClient;
+			}
+		}
+		return null;
+	}
+	
+	public int gethashMem(String key) {
+		return Math.abs(key.hashCode() % clientMap.size());
+	}
+	
+	public String get(String key) {
+		String value = "";
+		MClient mClient = randMClient(key.hashCode());
+		value = mClient.get(key);
+		return value;
+	}
+	
+	public boolean set(String key, String value) {
+		boolean result = false;
+		MClient mClient = leaderClient(key.hashCode());
+		result = mClient.set(key, value);
+		return result;
+	}
+}
--- rmemcached_client/src/main/java/com/sdp/server/ServerNode.java
+++ rmemcached_client/src/main/java/com/sdp/server/ServerNode.java
@@ -1,35 +1,21 @@
 package com.sdp.server;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ServerNode {
 	private int id;
 	private String host;
 	private int port;
-	private List<String> memcached;
 	
-	public ServerNode(String host, int port, String memcached) {
+	public ServerNode(String host, int port) {
 		this.host = host;
 		this.port = port;
-		this.memcached = new ArrayList<String>();
-		String[] memcachedList = memcached.split(",");
-		for (String mcNode : memcachedList) {
-			this.memcached.add(mcNode);
-		}
 	}
 	
-	public ServerNode(int id, String host, int port, String memcached) {
+	public ServerNode(int id, String host, int port) {
 		this.id = id;
 		this.host = host;
 		this.port = port;
-		this.memcached = new ArrayList<String>();
-		String[] memcachedList = memcached.split(",");
-		for (String mcNode : memcachedList) {
-			this.memcached.add(mcNode);
-		}
 	}
-	
+
 	public int getId() {
 		return id;
 	}
@@ -47,11 +33,5 @@ public class ServerNode {
 	}
 	public void setPort(int port) {
 		this.port = port;
-	}
-	public List<String> getMemcached() {
-		return memcached;
-	}
-	public void setMemcached(List<String> memcached) {
-		this.memcached = memcached;
 	}
 }
